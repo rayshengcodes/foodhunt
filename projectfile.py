f91c9beb691c9fe9505b36f8bfc4a68ec84369ac
KQ Lim <khengquanlim@gmail.com>
2018-11-11 01:28:29 +0800

    Whoops
    Sorry, i left my code in the projectfile, removing it!

--- projectfile.py
+++ projectfile.py
@@ -8,29 +8,3 @@ menu()
 
 pygame.quit()
 
-
-def budget():
-    while True:
-        try:
-            return float(input("What is your budget?"))
-        except ValueError:
-            print("Please type numerals.")
-
-# Search all canteens to return the food within the searched range
-def search_by_price():
-    pricefood = {}
-
-    howmuch = budget()
-    for i in datalist.canteendata:  # iterates canteens
-        food = []  # temporary list
-        for j in datalist.canteendata[i]['Food Price']:  # iterates food items
-            x = datalist.canteendata[i]['Food Price'][j]  # j is the dishes name
-
-            if x <= howmuch and x != 0:
-                food.append(j)
-        pricefood[i] = food
-    print("The food within your budget at this places are")
-    for key,val in pricefood.items():
-        print (key, ':', val)
-
-    return pricefood
